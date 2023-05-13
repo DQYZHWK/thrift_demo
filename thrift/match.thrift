8c710a29eba07ece106c6bf12362d64b9c8d8d2b
DQYZHWK <76473235+DQYZHWK@users.noreply.github.com>
2023-05-13 14:52:28 +0800

    add Match Server

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -1,6 +1,6 @@
 namespace cpp match_service
 
-struct user{
+struct User{
     1: i32 id,
     2: string name,
     3: i32 score
@@ -8,5 +8,5 @@ struct user{
 
 service Match{
     i32 add_user(1: User user, 2: string info),
-    i32 remove_user(1: User, 2: string info),
+    i32 remove_user(1: User user, 2: string info),
 }
